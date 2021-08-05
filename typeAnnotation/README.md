21f9fcd2c6c32874e36a0b95df73e1cda76735ac
Junior Martins <amjr.box@gmail.com>
2021-08-05 06:58:02 -0300

    Create type boolean

---
+++ typeAnnotation/README.md
@@ -0,0 +1,14 @@
+<h1 align="center">Typescript</h1>
+<p align="center">Repository for studying TypeScript, based on the Glaucia Lemos course - <a href="https://www.youtube.com/watch?v=u7K1sdnCv5Y&list=PLb2HQ45KP0Wsk-p_0c6ImqBAEFEY-LU9H">TypeScript - Zero to Hero</a></p>
+
+<p align="right">
+<img src="https://img.shields.io/badge/typescript-4.3.5-3178C6?style=?style=flat-square&logo=appveyor&logo=TypeScript"/>
+<img src="https://img.shields.io/badge/node.js-14.16.1-339933?style=?style=flat-square&logo=appveyor&logo=Node.js"/>
+<img src="https://img.shields.io/badge/yarn-1.22.10-2C8EBB?style=?style=flat-square&logo=appveyor&logo=Yarn"/>
+</p>
+
+## What is needed? (O que é necessário?)
+- [VSCode](https://code.visualstudio.com/)
+- [NodeJS](https://nodejs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- NPM or [Yarn](https://yarnpkg.com/)
---
+++ typeAnnotation/boolean/index.ts
@@ -0,0 +1,40 @@
+// How it's done in Javascript
+/**
+ * let taskCompleted = true;
+ */
+
+// How it's done in Typescript
+let taskCompleted: boolean = true;
+
+console.log(taskCompleted);
+
+//return: boolean
+console.log(typeof taskCompleted);
+
+if (typeof taskCompleted === 'boolean') {
+  console.log('taskCompleted is a boolean');
+} else {
+  console.log(
+    'taskCompleted is not a boolean, message_2 is a ' + typeof message_1
+  );
+}
+
+console.log('-------------x-------------');
+
+/*
+// How it's done in Typescript
+let incompleteTask: boolean = 'false';
+
+console.log(incompleteTask);
+
+//return: string
+console.log(typeof incompleteTask);
+
+if (typeof incompleteTask === 'boolean') {
+  console.log('incompleteTask is a boolean');
+} else {
+  console.log(
+    'incompleteTask is not a boolean, incompleteTask is a ' + typeof message_1
+  );
+}
+*/
