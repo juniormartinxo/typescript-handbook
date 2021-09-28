12ed2445be9247a4d5b6a47a6c6a118f376e5330
Junior Martins <amjr.box@gmail.com>
2021-09-28 19:23:13 -0300

    Add Type Utilities

---
+++ mini-course-willian-justen/module10-TypeUtilities.ts
@@ -0,0 +1,13 @@
+type Todo = {
+  title: string
+  description: string
+  completed: boolean
+}
+
+const todo: Todo = {
+  title: 'Write TypeScript',
+  description: 'Learn TypeScript',
+  completed: false,
+}
+
+console.log(todo)
