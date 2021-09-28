1df2f2b5d21e31f8d6569e16005bfe29fb3df21b
Junior Martins <amjr.box@gmail.com>
2021-09-28 20:20:21 -0300

    Update module-10

--- mini-course-willian-justen/module10-TypeUtilities.ts
+++ mini-course-willian-justen/module10-TypeUtilities.ts
@@ -4,10 +4,37 @@ type Todo = {
   completed: boolean
 }
 
-const todo: Todo = {
+// Readonly
+const todo: Readonly<Todo> = {
   title: 'Write TypeScript',
   description: 'Learn TypeScript',
   completed: false,
 }
 
 console.log(todo)
+
+//todo.completed = true
+
+// Partial
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+  return { ...todo, ...fieldsToUpdate }
+}
+
+const todo2: Todo = updateTodo(todo, {
+  title: 'Learn TypeScript',
+  description: 'Learn TypeScript',
+  completed: true,
+})
+
+console.log(todo2)
+
+// Pick
+type TodoPreview = Pick<Todo, 'title' | 'completed'>
+
+const todo3: TodoPreview = {
+  title: 'Write TypeScript',
+  completed: false,
+}
+
+// Omit
+type TodoPreview2 = Omit<Todo, 'description'>
